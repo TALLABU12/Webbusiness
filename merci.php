71ec8213deffe8715fc2aaf1c7beb115e3dea116
TALLABU12 <tallaboubacar165@gmail.com>
2025-02-26 17:36:55 +0000

    Add files via upload

---
+++ merci.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Merci</title>
+</head>
+<body>
+    <h1>Merci de nous avoir contactés</h1>
+    <p>Nous avons bien reçu votre message et vous répondrons dans les plus brefs délais.</p>
+</body>
+</html>
