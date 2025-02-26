423c9b645ba3e893db73a1aa45abb2e891f4752c
TALLABU12 <tallaboubacar165@gmail.com>
2025-02-26 22:24:41 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Récupérer les données du formulaire
     $name = $_POST['name'];
     $email = $_POST['email'];
